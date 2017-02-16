7ca82987f6b7cd0092f758544029cea30ad98cc4
IvanAndreyshev-NOTEBOOK <vanyha360@yandex.ru>
2017-02-16 17:44:32 +0300

    Реализация алгоритма

--- Task1/Task1/Task1.cpp
+++ Task1/Task1/Task1.cpp
@@ -1,67 +1,140 @@
 #include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <algorithm>
-#include <string>
-
-bool ReadMatrix(vector<vector<int>>& matrix, ifstream& input);
-void ProcessMatrix(vector<vector<int>> matrix);
-
-int main()
+void ReadLineFromFileToBoolVector(ifstream& file, vector<bool>& vect, char ifTrue)
 {
-	ifstream input("input.txt");
-	vector<vector<int>> matrix;
-	
-	ReadMatrix(matrix, input);
-
-	for (size_t i = 0; i != matrix.size(); i++)
+	string line;
+	getline(file, line);
+	stringstream stream(line);
+	char elem;
+	while (stream >> elem)
 	{
-		for (size_t j = 0; j != matrix[0].size(); j++)
+		vect.push_back(elem == ifTrue);
+	}
+}
+
+template<typename T>
+void PrintVector(vector<T> const& vect)
+{
+	copy(vect.begin(), vect.end(), ostream_iterator<T>(cout, " "));
+	cout << endl;
+}
+
+template<typename T>
+void PrintMatrix(vector<vector<T>> const& boolMatrix)
+{
+	for (size_t i = 0; i < boolMatrix.size(); ++i)
+	{
+		PrintVector(boolMatrix[i]);
+
+		for (size_t j = 0; j < boolMatrix[i].size(); ++j)
 		{
-			cout << matrix[i][j];
+			char symbol = (boolMatrix[i][j]) ? '1' : '0';
+			cout << symbol;
 		}
 		cout << "\n";
 	}
+}
 
-	ProcessMatrix(matrix);
+void FindConnection(vector<vector<bool>>& matrix, size_t y, size_t& connectionCount, vector<size_t>& connectionSizes, size_t& yToStop, set<size_t>& checkedRows)
+{
+	checkedRows.insert(y);
+	for (size_t x = 0; x < matrix[y].size(); ++x)
+	{
+		if (matrix[y][x])
+		{
+			if (yToStop == SIZE_MAX)
+			{
+				yToStop = y;
+			}
+			connectionCount++;
+			matrix[y][x] = false;
+			if (x == yToStop)
+			{
+				connectionSizes.push_back(connectionCount);
+				break;
+			}
+			FindConnection(matrix, x, connectionCount, connectionSizes, yToStop, checkedRows);
+			break;
+		}
+	}
+}
+
+void ReadMatrixSize(ifstream& file, size_t& size)
+{
+	string line;
+	getline(file, line);
+	stringstream stream(line);
+	stream >> size;
+}
+
+vector<vector<bool>> ReadMatrix(ifstream& file)
+{
+	size_t matrixCollumsCount = 0;
+	ReadMatrixSize(file, matrixCollumsCount);
+	vector<vector<bool>> matrix(matrixCollumsCount);
+
+	for (size_t i = 0; i < matrixCollumsCount; ++i)
+	{
+		ReadLineFromFileToBoolVector(file, matrix[i], '1');
+	}
+
+	return matrix;
+}
+
+vector<size_t> FindCycles(vector<vector<bool>> const& sourceMatrix)
+{
+	auto matrix = sourceMatrix;
+	vector<size_t> connectionSizes;
+	set<size_t> checkedRows;
+
+	for (size_t y = 0; y < matrix.size(); ++y)
+	{
+		if (checkedRows.find(y) == checkedRows.end())
+		{
+			size_t connectionCount = 0;
+			size_t yToStop = SIZE_MAX;
+			FindConnection(matrix, y, connectionCount, connectionSizes, yToStop, checkedRows);
+		}
+	}
+	sort(connectionSizes.begin(), connectionSizes.end());
+
+	return connectionSizes;
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "");
+
+	ifstream input0(argv[1]);
+	ifstream input1(argv[2]);
+
+	auto firstMatrix = ReadMatrix(input0);
+	auto connectionSizesFirst = FindCycles(firstMatrix);
+
+	auto secondMatrix = ReadMatrix(input1);
+	auto connectionSizesSecond = FindCycles(secondMatrix);
+
+	PrintVector(connectionSizesFirst);
+	PrintVector(connectionSizesSecond);
+
+	if (connectionSizesFirst == connectionSizesSecond)
+	{
+		cout << "Графы изоморфны" << endl;
+	}
+	else
+	{
+		cout << "Изоморфизм отсутствует!" << endl;
+	}
 
 	return 0;
-}
-
-bool ReadMatrix(vector<vector<int>>& matrix, ifstream& input)
-{
-	size_t numCount = 0;
-
-	input >> numCount;
-
-	for (int i = 0; i != numCount; i++)
-	{
-		vector<int> numbers;
-		matrix.push_back(numbers);
-		for (int j = 0; j != numCount; j++)
-		{
-			char symbol;
-			input >> symbol;
-			int num = (symbol == '1') ? 1 : 0;
-			matrix[i].push_back(num);
-		}
-	}
-
-
-
-	return true;
-}
-
-void ProcessMatrix(vector<vector<int>> matrix)
-{
-	for (size_t row = 0; row < matrix.size(); row++)
-	{
-		for (size_t colls = 0; colls < matrix[row].size(); colls++)
-		{
-		}
-	}
 }
